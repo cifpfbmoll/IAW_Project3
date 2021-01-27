16f4c99e444e4fdca17f172dfe22674d38dc9df7
miquelcabot <miquel.cabot@gmail.com>
2021-01-27 17:20:23 +0100

    Add google login and mongodb connection

--- main.js
+++ main.js
@@ -1,23 +1,63 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/db')
+
+// Load config
+dotenv.config({ path: './config/config.env' })
+
+// Passport config
+require('./config/passport')(passport)
+
+connectDB()
 
 const app = express()
 
-// Static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-const PORT = process.env.PORT || 3000;
-
 // Handlebars
-app.engine('.hbs', exphbs({ 
-  defaultLayout: 'main', 
-  extname: '.hbs'
-}));
-app.set('view engine', '.hbs');
+app.engine(
+  '.hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+)
+app.set('view engine', '.hbs')
 
-//Routes
+// Sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
 app.use('/', require('./routes/index'))
- 
-app.listen(PORT, 
-  () => console.log(`Server running on port ${PORT}...`))
+app.use('/auth', require('./routes/auth'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+
+const Restaurant = require('../models/Restaurant')
 
 //desc Login/Landin page
 //@route get /
-router.get('/', (req, res) => {
-    res.render('home')
-});
+router.get('/', async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find({ }).lean()
+    res.render('home', {
+      restaurants,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
 
-module.exports = router;
+module.exports = router
